945184ef41b5213f35fd4857a057770580b47188
David Okun <david@okun.io>
2019-08-03 11:48:24 -0500

    Removing extra stuff

--- Sources/EmojiJournalCore/EmojiJournalCore.swift
+++ Sources/EmojiJournalCore/EmojiJournalCore.swift
@@ -17,32 +17,3 @@ public struct JournalEntry: Codable {
   public var emoji: String
   public var date: Date
 }
-
-#if canImport(UIKit)
-import UIKit
-
-extension UIColor {
-  var rgba: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
-    var red: CGFloat = 0
-    var green: CGFloat = 0
-    var blue: CGFloat = 0
-    var alpha: CGFloat = 0
-    getRed(&red, green: &green, blue: &blue, alpha: &alpha)
-
-    return (red, green, blue, alpha)
-  }
-}
-#if canImport(SwiftUI)
-import SwiftUI
-
-@available(iOS 13.0, *)
-extension Color {
-  init(uiColor: UIColor) {
-    self.init(red: Double(uiColor.rgba.red),
-              green: Double(uiColor.rgba.green),
-              blue: Double(uiColor.rgba.blue),
-              opacity: Double(uiColor.rgba.alpha))
-   }
-}
-#endif
-#endif
